55e12ad7837358e9c1d113b3e013061642f9e208
carloscordova.dev <cordovamanuel22@icloud.com>
2024-04-10 07:52:53 -0700

    add lesson2

---
+++ lesson02/welcome.py
@@ -0,0 +1,15 @@
+line1 = "****************************************************************"  # header / footer
+line2 = "*                                                              *"  # re-use
+line3 = "*                    WELCOME!                                  *"
+line4 = "****************************************************************"
+
+# start with a blank line
+print('')
+print(line1)
+print(line2)
+print(line3)
+print(line2)
+print(line1)
+print('')
+
+
